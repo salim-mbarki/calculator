88a1ba7b3ab7367f54eeac21b28ad3dcba80e1a4
Salim Mbarki <salim.mbarki.mail@gmail.com>
2026-07-24 18:43:51 +0100

    Add operate functionality

--- logic.js
+++ logic.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, b, operator) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = substract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+    }
+    return result;
+}
