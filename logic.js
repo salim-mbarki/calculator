1bd1ce50b638b9e898e03183a8f4e2f4bb732114
Salim Mbarki <salim.mbarki.mail@gmail.com>
2026-07-27 23:29:31 +0100

    Add core calculator logic

--- logic.js
+++ logic.js
@@ -32,3 +32,40 @@ function operate(a, b, operator) {
     }
     return result;
 }
+
+let a = '';
+let b = '';
+let operator = '';
+
+const buttons = Array.from(document.querySelectorAll('button'));
+const display = document.querySelector('.display');
+
+buttons.forEach(button => {
+    button.value = button.textContent;
+    button.addEventListener('click', (e) => {
+        const value = e.target.value;
+        const isOperator = value === '+' || value === '-' || value === '/' ||
+            value === '*';
+        if (a && isOperator) {
+            operator = value;
+            if (a && b) {
+                a = (operate(+a, +b, operator));
+                b = '';
+                display.textContent = a;
+            }
+        }
+        if (!operator && !isOperator) {
+            a += value;
+            display.textContent = a;
+        }
+        if (operator && !isOperator && value !== '=') {
+            b += value;
+            display.textContent = b;
+        }
+        if (value === '=' && a && b && operator) {
+            a = (operate(+a, +b, operator));
+            b = '';
+            display.textContent = a;
+        }
+    })
+})
