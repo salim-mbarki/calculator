786ba7dd1c0a44d06eaf37152ae70cd9a4c2deff
Salim Mbarki <salim.mbarki.mail@gmail.com>
2026-07-28 21:19:55 +0100

    Refactor calculator logic into helper functions

--- logic.js
+++ logic.js
@@ -33,39 +33,57 @@ function operate(a, b, operator) {
     return result;
 }
 
-let a = '';
-let b = '';
-let operator = '';
+let leftNumber = '';
+let rightNumber = '';
+let operator;
 
 const buttons = Array.from(document.querySelectorAll('button'));
 const display = document.querySelector('.display');
 
 buttons.forEach(button => {
-    button.value = button.textContent;
-    button.addEventListener('click', (e) => {
-        const value = e.target.value;
-        const isOperator = value === '+' || value === '-' || value === '/' ||
-            value === '*';
-        if (a && isOperator) {
-            operator = value;
-            if (a && b) {
-                a = (operate(+a, +b, operator));
-                b = '';
-                display.textContent = a;
-            }
-        }
-        if (!operator && !isOperator) {
-            a += value;
-            display.textContent = a;
-        }
-        if (operator && !isOperator && value !== '=') {
-            b += value;
-            display.textContent = b;
-        }
-        if (value === '=' && a && b && operator) {
-            a = (operate(+a, +b, operator));
-            b = '';
-            display.textContent = a;
-        }
-    })
+    button.addEventListener('click', handleClick);
 })
+
+function handleClick(e) {
+    const value = e.target.textContent;
+    if (value === 'CE') {
+        clearCalculator();
+    } else if (['+', '-', '*', '/'].includes(value)) {
+        handleOperator(value);
+    } else if (value === '=') {
+        calculate();
+    } else {
+        handleNumber(value);
+    }
+}
+
+function clearCalculator() {
+    leftNumber = '';
+    rightNumber = '';
+    display.textContent = 0;
+}
+
+function handleOperator(value) {
+    if (rightNumber) {
+        leftNumber = (operate(+leftNumber, +rightNumber, operator));
+        rightNumber = '';
+        display.textContent = leftNumber;
+    }
+    operator = value;
+}
+
+function calculate() {
+    leftNumber = (operate(+leftNumber, +rightNumber, operator));
+    rightNumber = '';
+    display.textContent = leftNumber;
+}
+
+function handleNumber(value) {
+    if (operator) {
+        rightNumber += value;
+        display.textContent = rightNumber;
+    } else {
+        leftNumber += value;
+        display.textContent = leftNumber;
+    }
+}
